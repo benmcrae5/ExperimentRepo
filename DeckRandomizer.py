e287fa5eb760a32aea6c53d92017164292f8cd7c
Ben McRae <benmcrae5@yahoo.com>
2024-05-08 19:31:01 -0700

    Add notation for clarification

--- DeckRandomizer.py
+++ DeckRandomizer.py
@@ -1,15 +1,18 @@
 import random
 import math
 
+#Starting Deck is a standard 52 card deck, using simple numbers from 0 to 51
 StartDeck = list(range(52))
-#print(StartDeck)
 
+#Provide the number of sides for a dice, get a random roll.
+#TO BE MOVED TO ANOTHER FILE
 def rollDice(sides, report=False):
     result = int(random.random()*sides)
     if report:
         print(result)
     return result
 
+#alternate putting cards into piles, then stack the piles in a random order. 
 def stackShuffle(deck, piles=5):
     newDeck=[]
     pileArray = list(range(piles))
@@ -21,6 +24,7 @@ def stackShuffle(deck, piles=5):
             idx = idx + piles
     return newDeck    
 
+#alternate putting cards on top then bottom using variable card amounts
 def shuffleTopBottom(deck, maxCards=5):
     newDeck = []
     idx = 0
@@ -40,6 +44,7 @@ def shuffleTopBottom(deck, maxCards=5):
             continue
     return newDeck
 
+#Simple shuffler using python random
 def randomShuffler(deck):
     oldDeck = deck.copy()
     newDeck = []
@@ -49,6 +54,7 @@ def randomShuffler(deck):
         oldDeck.pop(randomNum)
     return newDeck
 
+#the "perfect" shuffle - split deck into two piles, alternate cards in a standard shuffle
 def standardShuffle(deck):
     newDeck = []
     halfDeck = math.floor(len(deck)/2)
@@ -58,7 +64,8 @@ def standardShuffle(deck):
     if len(deck)%2 == 1:
         newDeck.append(deck[len(deck)-1])
     return newDeck
-        
+
+#Runs a single shuffle method for a given number of iterations
 def runMultiShuffle(deck, shuffleMethod, iterations, reportBool=False):
     #print("Start Multi-Shuffle")
     newDeck = deck.copy()
@@ -69,6 +76,7 @@ def runMultiShuffle(deck, shuffleMethod, iterations, reportBool=False):
             print(newDeck)
     return newDeck
 
+#Runs multiple shuffle methods in a sequence at random
 def runChainShuffle(deck, iterations, reportBool=False):
     #run multiple shuffle types together
     #functions may only have one parameter (deck), but can have multiple parameters if pre-set
@@ -89,6 +97,7 @@ def runChainShuffle(deck, iterations, reportBool=False):
 
 #TEST AREA
 
+#print(StartDeck)
 #print(runChainShuffle(StartDeck, 4, True))
 #print(runMultiShuffle(StartDeck, shuffleTopBottom, 4, False))
 #print(runMultiShuffle(StartDeck, standardShuffle, 4, False))
