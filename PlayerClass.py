a8a593ca25296b146b7ce50b69009e396929a6ae
Ben McRae <benmcrae5@yahoo.com>
2024-05-13 14:37:18 -0700

    Add methods to Player Class

--- PlayerClass.py
+++ PlayerClass.py
@@ -1,15 +1,22 @@
+import DeckRandomizer as DeckR
+
 #Generic player creation class
 class Player:
-    def __init__(self, name, cardHand = [], points = 0, life = 0, priority=0):
+    def __init__(self, name, cardHand = [], points = 0, life = 0, priority=0, deck=[]):
         self.name = name
         self.cardHand = cardHand
         self.points = points
         self.life = life
         self.priority = priority
+        self.deck = deck
 
     def __str__(self):
         return f'{self.name}'
+    #note to self: repr prints when printing lists, str prints in print statments
+    def __repr__(self):
+        return f'Player(\'{self.name}\', {self.points}, {self.life}, {self.priority}) :: {self.cardHand}'
 
+    #comparitive functions between Player Objects based on self.priority
     def __eq__(self, other):
         return self.name == other.name and self.priority == other.priority
     def __lt__(self, other):
@@ -20,3 +27,27 @@ class Player:
         return self.priority > other.priority
     def __ge__(self, other):
         return self.priority >= other.priority
+
+    #Player Functions
+    def shuffleDeck(self):
+        self.deck = DeckR.runChainShuffle(self.deck, 3)
+
+    def addCardToDeck(self, card):
+        self.deck.appent(card)
+
+    def adjustLife(self, amount):
+        self.life = self.life + amount
+
+    def adjustPoints(self, amount):
+        self.points = self.points + amount
+
+    def drawCards(self, numCards):
+        for x in range(numCards):
+            if len(self.deck) <= 0:
+                print(f'Not enough cards. You drew {x} instead')
+                break
+            self.cardHand.append(self.deck.pop())
+
+    
+
+    
--- TestExec.py
+++ TestExec.py
@@ -4,30 +4,21 @@ import PlayerClass as PC
 
 #TEST FILE
 
-#print(DeckR.StartDeck)
-#print(DeckR.runChainShuffle(StartDeck, 4, True))
-#print(DeckR.runMultiShuffle(StartDeck, shuffleTopBottom, 4, False))
-#print(DeckR.runMultiShuffle(StartDeck, standardShuffle, 4, False))
-#print(DeckR.runMultiShuffle(StartDeck, stackShuffle, 4, True))
-#print(DeckR.randomShuffler(StartDeck))
-#print(DeckR.standardShuffle(StartDeck))
-#print(DiceR..rollDice(6))
-#print(DiceR.riskDiceBattle())
-P1 = PC.Player("adam", priority = 7)
-P2 = PC.Player("bob", priority = 4)
-P3 = PC.Player("carole", priority = 9)
-P4 = PC.Player("dianne", priority = 5)
+p1 = PC.Player("adam", [1,2,3,4], 10, 40, 7, ['a','b','c','d'])
 
-pa = [P1, P2, P3, P4]
-for x in pa:
-    print(str(x))
-pa.sort()
-for x in pa:
-    print(str(x))
-print(P1)
-print(P1>P2)
+p1.drawCards(3)
+print(p1.deck)
+print(p1.cardHand)
+p1.drawCards(2)
+print(p1.deck)
+print(p1.cardHand)
 
-for x in range(20):
-    y=0
-    #print(DiceR.dndSkillCheck(15, disadv=True, adv=True, report=True))
-    #DiceR.singleDiceBattle(20)
+p1.adjustPoints(1)
+print(p1.points)
+p1.adjustPoints(-5)
+print(p1.points)
+
+p1.adjustLife(1)
+print(p1.life)
+p1.adjustLife(-5)
+print(p1.life)
