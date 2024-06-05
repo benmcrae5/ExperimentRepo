edc844f3f3fe992a3395e08134747d200d63c10f
Ben McRae <benmcrae5@yahoo.com>
2024-06-05 13:56:19 -0700

    add attack function to Character Class

--- CharacterClass.py
+++ CharacterClass.py
@@ -5,17 +5,24 @@ class Character:
     def __init__(self, name, cost = {}, move = 1, attack = 1, defense = 1, health = 1, abilities = {}, player = None):
             self.name = name
             self.cost = cost            #Cost is a Dictionary object with {"Resource": #}
-            self.move = move
-            self.attack = attack
-            self.defense = defense
-            self.health = health
+            self.move = int(move)
+            self.attack = int(attack)
+            self.defense = int(defense)
+            self.health = int(health)
             self.abilities = abilities  #abilities are in a Dictionary object with {"abilityName": abilityFunction}
             self.player = player
             self.id = uuid.uuid4().int  #Unique Identifier for created character
+            self.status = "ok"
 
     def __repr__(self):
         return self.name
     def __str__(self):
         return self.name
 
-    
+    def _attack(self, target):
+        print(f"{self} is attacking!")
+        target.health = target.health - self.attack
+        print(f"{self} deals {self.attack} damage to {target}")
+        if target.health <= 0:
+            target.status = "dead"
+            print(f'{target} has died!')
--- TestExec.py
+++ TestExec.py
@@ -13,9 +13,15 @@ p1 = PC.Player("Adam", [1,2,3,4], 10, 40, 7, ['a','b','c','d',5,6,7,8], {"Wood":
 p2 = PC.Player("Betty", ['a','b','c','d'], 10, 40, 7, ['e','f','g','h',1,2,3,4], {})
 
 
-char1 = CharCl.Character("Elemental", {"food": 10, "magic": 5}, 6, 4, 2, 5)
+char1 = CharCl.Character("Elemental",
+                         {"food": 10, "magic": 5}, 6, 4, 2, 5)
+char2 = CharCl.Character("Goblin",
+                         {"food": 2}, 1, 1, 1, 1)
 
-print(char1)
-print(char1.name, char1.cost, char1.move, char1.attack, char1.defense, char1.health)
-print(char1.id)
+z = char1
+print(z.name, z.cost, z.move, z.attack, z.defense, z.health)
+z = char2
+print(z.name, z.cost, z.move, z.attack, z.defense, z.health)
+print(type(char1), type(char2))
 
+char1._attack(char2)
